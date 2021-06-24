355ab80a46c4e12eb1dea187c087a8244ebc259c
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2021-06-24 14:33:00 -0700

    Base elements have been implemented but no functionality has been created

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,15 @@
+
+function Login() {
+
+  // component return function
+  return (
+    <div>
+      <span>Login</span>
+      <input type='text'></input>
+      <input type='password'></input>
+      <button>Log in</button>
+    </div>
+  );
+}
+
+export default Login;
