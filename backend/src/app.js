453fc9095e259f687fe5f5311628409073352b08
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2021-06-29 00:40:32 -0700

    Added an OpenAPI validator with a method of testing it

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,46 @@
 // create an express application
 const express = require('express');
-const { getBlogPosts } = require('./blogposts');
-const app = express(); // this is exported
+const app = express(); // this is exported from this source file
 
-// paths
+// api validation setup
+const OpenApiValidator = require('express-openapi-validator'); // the openapi validator
+const swaggerUi = require('swagger-ui-express'); // the swagger ui for verifying the api
+const YAML = require('js-yaml'); // used to open and read the api validator doc
+const fs = require('fs'); // used when loading the openapi spec
+const apiSpec = YAML.load(fs.readFileSync('src/api/openapi.yaml')); // loading the api specification
+
+// blog post route code imported here
+const { getBlogPosts } = require('./blogposts');
+
+// == middleware == //
+/**
+ * Open API validator middleware setup 
+ * (using documentation from the following: 
+ * https://www.npmjs.com/package/express-openapi-validator
+ * https://www.npmjs.com/package/swagger-ui-express
+ * https://www.npmjs.com/package/js-yaml
+ * )
+ */
+// swagger UI in api-docs
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(apiSpec));
+// installing openapi validation middleware
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: apiSpec,
+    validateRequests: true,
+    validateResponses: true
+  })
+);
+// error handler (from the express-openapi-validator documentation)
+app.use((err, req, res, next) => {
+  // format error
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
+
+// == paths == //
 /**
  * Default path currently just returns hello world
  */
