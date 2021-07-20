b15a005b4d23082c6d9fb0e5e13dc06ade2afb94
Emmanuel Butor <emmanuelbutor.mehs@gmail.com>
2021-07-19 22:21:43 -0700

    Re-added the right-to-left formatting (was removed unintentionally)

--- frontend/src/components/PostEditor.js
+++ frontend/src/components/PostEditor.js
@@ -46,6 +46,7 @@ export default function PostEditor(props) {
     [{ 'list': 'ordered'}, { 'list': 'bullet' }],
     [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
     [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
   
     [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
